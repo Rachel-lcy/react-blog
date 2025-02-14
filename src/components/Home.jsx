7326ec9a78acaee87a2cd86cdf64cda6dcf2e06e
Rachel <hurui.lcy@gmail.com>
2025-02-13 21:35:41 -0700

    Reusing Components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,8 @@ const Home = () => {
 
   return (
     <div className="home">
-     <BlogList blogs = {blogs} title = 'All blogs'/>
+     <BlogList blogs = {blogs} title = 'All blogs!'/>
+     <BlogList blogs = {blogs.filter((blog) => blog.author === "mario")} title = "Rachel's Blog" />
     </div>
    );
 }
